787691f8265e49a57b56e9e8b5f3398604d1330a
Nikhil L <nikhil89515@gmail.com>
2021-05-31 10:58:50 +0530

    Create code_camp_3_test.py

---
+++ Code_camp_3/code_camp_3_test.py
@@ -0,0 +1,10 @@
+
+import unittest
+from unittest import mock
+
+class CustomerBillTesting(unittest.TestCase):
+    def test_calculate_customer_bill(self):
+        pass
+
+if __name__ == '__main__':
+	unittest.main()
